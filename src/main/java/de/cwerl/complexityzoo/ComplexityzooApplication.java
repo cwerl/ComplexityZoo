b05d94f635a646b20681c29db2700aa3c2daa998
cwerl <christopher.werl1@gmail.com>
2022-04-05 17:46:35 +0200

    Spring initialization

---
+++ src/main/java/de/cwerl/complexityzoo/ComplexityzooApplication.java
@@ -0,0 +1,13 @@
+package de.cwerl.complexityzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexityzooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplexityzooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/cwerl/complexityzoo/ComplexityzooApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cwerl.complexityzoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplexityzooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
