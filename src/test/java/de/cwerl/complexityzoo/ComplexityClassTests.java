7850e99cb441377208d7a82e6862740d1c1177e8
cwerl <christopher.werl1@gmail.com>
2022-04-18 17:47:49 +0200

    test improvements and search + view test

--- src/test/java/de/cwerl/complexityzoo/ComplexityClassTests.java
+++ src/test/java/de/cwerl/complexityzoo/ComplexityClassTests.java
@@ -3,8 +3,8 @@ package de.cwerl.complexityzoo;
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 
@@ -28,43 +28,85 @@ public class ComplexityClassTests {
 	@Autowired
 	private ComplexityClassRepository classRepository;
 
+	// Attribute keywords
+	private static final String ATTR_CLASSLIST = "classes";
+	private static final String ATTR_ID = "id";
+	private static final String ATTR_CLASSNAME = "name";
+	private static final String ATTR_DESCR = "description";
+
+	@Test
+	@Transactional
+	public void viewClassTest() throws Exception {
+		ComplexityClass c = new ComplexityClass();
+		c.setName("A");
+		classRepository.save(c);
+
+		this.mockMvc.perform(
+			get("/classes/{id}", c.getId()))
+			.andExpect(status().isOk());
+	}
+
     @Test
 	@Transactional
     public void createClassTest() throws Exception {
         this.mockMvc.perform(
             post("/classes/add")
-			.param("name", "testName")
-			.param("description", "testDescription")
+			.param(ATTR_CLASSNAME, "name")
+			.param(ATTR_DESCR, "description")
 		);
 		this.mockMvc.perform(get("/classes"))
-		.andExpect(model().attribute("classes", hasItem(
+		.andExpect(model().attribute(ATTR_CLASSLIST, hasItem(
 			allOf(
-				hasProperty("name", is("testName")),
-				hasProperty("description", is("testDescription"))
+				hasProperty(ATTR_CLASSNAME, is("name")),
+				hasProperty(ATTR_DESCR, is("description"))
 			)
-		)));
+		)))
+		.andExpect(status().isOk());
     }
 
 	@Test
 	@Transactional
     public void deleteClassFilledDatabaseTest() throws Exception {
 		ComplexityClass c1 = new ComplexityClass();
-		c1.setName("name");
+		c1.setName("name1");
 		classRepository.save(c1);
 
 		ComplexityClass c2 = new ComplexityClass();
-		c2.setName("name");
+		c2.setName("name2");
 		classRepository.save(c2);
 
         this.mockMvc.perform(
             MockMvcRequestBuilders.delete("/classes/{id}/delete", c1.getId())
-		);
+		).andExpect(status().is(302));
+
 		this.mockMvc.perform(get("/classes"))
-		.andExpect(model().attribute("classes", not(hasItem(
-			hasProperty("id", is(c1.getId()))
+		.andExpect(model().attribute(ATTR_CLASSLIST, not(hasItem(
+			hasProperty(ATTR_ID, is(c1.getId()))
 		))))
-		.andExpect(model().attribute("classes", hasItem(
-			hasProperty("id", is(c2.getId()))
-		)));
+		.andExpect(model().attribute(ATTR_CLASSLIST, hasItem(
+			hasProperty(ATTR_ID, is(c2.getId()))
+		)))
+		.andExpect(status().isOk());
     }
+
+	@Test
+	@Transactional
+	public void searchClassTest() throws Exception {
+		ComplexityClass c1 = new ComplexityClass();
+		c1.setName("A");
+		classRepository.save(c1);
+
+		ComplexityClass c2 = new ComplexityClass();
+		c2.setName("B");
+		classRepository.save(c2);
+
+		this.mockMvc.perform(get("/classes/search?q=A"))
+		.andExpect(model().attribute(ATTR_CLASSLIST, hasItem(
+			hasProperty(ATTR_CLASSNAME, is(c1.getName()))
+		)))
+		.andExpect(model().attribute(ATTR_CLASSLIST, not(hasItem(
+			hasProperty(ATTR_CLASSNAME, is(c2.getName()))
+		))))
+		.andExpect(status().isOk());
+	}
 }
